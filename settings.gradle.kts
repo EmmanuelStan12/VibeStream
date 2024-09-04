28cc6f568d82182aa5d7f8039e916735afcd22ed
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2024-09-04 11:51:15 +0100

    task: Initialized frontend and backend boiler plate settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 rootProject.name = "VibeStream"
-
+include("features:auth")
+include("app")
+include("security")
+include("core:api")
